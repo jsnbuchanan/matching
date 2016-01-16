1a7d6e519f1ff4f0985a5cf21f00034bea8303d2
jsnbuchanan <jsnbuchanan@gmail.com>
2016-01-16 09:38:46 -0700

    Added defaulting of profile image where none is provided on the results view. Also added a bunch of sample images.

--- match/views.py
+++ match/views.py
@@ -1,5 +1,9 @@
+import logging
+
 from django.shortcuts import render
 from django.http import HttpResponse
+
+logger = logging.getLogger(__name__)
 
 christina = {'name':'Christina',
                        'description':'sexy funny kitty wrangler',
@@ -7,20 +11,50 @@ christina = {'name':'Christina',
                                  'img/profiles-samples/gilmores.jpg',
                                  'img/profiles-samples/kitties.jpg']}
 
-jason = {'name':'Jason','description':'robust healthy male'}
-ian = {'name':'Ian','description':'wickedly smart beneficent mastermind'}
-amy = {'name':'Amy','description':'amazon warrior princess'}
+jason = {'name':'Jason','description':'robust healthy male',
+         # 'images':['img/profiles-samples/jason-potion.jpg',
+         #           'img/profiles-samples/jason-warlord.jpg',
+         #           'img/profiles-samples/jason-butt.jpg']
+         }
+
+ian = {'name':'Ian','description':'wickedly smart beneficent mastermind',
+       'images':['img/profiles-samples/ian-fire.jpg',
+                 'img/profiles-samples/ian-frog.jpg',
+                 'img/profiles-samples/ian-color.jpg',
+                 'img/profiles-samples/ian-suckers.jpg']
+      }
+amy = {'name':'Amy','description':'amazon warrior princess',
+       'images':['img/profiles-samples/amy-kiss.jpg',
+                 'img/profiles-samples/amy-wifi.jpg',
+                 'img/profiles-samples/amy-savanah.jpg']
+      }
 
 # Create your views here.
+
+
 def home(request):
     return render(request, "match/search.html")
+
 
 def profiles(request):
     return render(request, "match/profiles.html", {'profile':christina})
 
+
 def profile(request):
     return render(request, "match/profile.html", {'profile':christina})
 
+
 def results(request):
-    return render(request, "match/results.html", {'profiles':[christina, ian, jason, amy]});
+    match_profiles = {'profiles':[christina, ian, jason, amy]}
+
+    for profile in match_profiles['profiles']:
+
+        if 'images' not in profile.keys():
+
+            profile['images'] = ['/static/img/profile.jpg']
+
+            # the following is an example of logging
+            logger.debug('The %s profile was assigned a generic thumbnail' % (profile['name']))
+
+    return render(request, "match/results.html", match_profiles)
 
